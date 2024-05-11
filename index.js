47b1b4bb6c9d2145d8c7839f3c7509a31cf2f6c7
Daniel Pérez Arriagada <danielperez.dapa@gmail.com>
2024-05-11 01:10:53 -0400

    busboy version updated

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const Busboy = require('busboy');
     }
  */
 const parse = (event) => new Promise((resolve, reject) => {
-    const busboy = new Busboy({
+    const busboy = Busboy({
         headers: {
             'content-type': event.headers['content-type'] || event.headers['Content-Type']
         }
@@ -30,17 +30,16 @@ const parse = (event) => new Promise((resolve, reject) => {
         files: []
     };
 
-    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+    busboy.on('file', (fieldname, file, info) => {
+        const { filename, encoding, mimeType } = info;
         const uploadFile = {};
 
         file.on('data', data => {
             uploadFile.content = data;
-        });
-
-        file.on('end', () => {
+        }).on('close', () => {
             if (uploadFile.content) {
                 uploadFile.filename = filename;
-                uploadFile.contentType = mimetype;
+                uploadFile.contentType = mimeType;
                 uploadFile.encoding = encoding;
                 uploadFile.fieldname = fieldname;
                 result.files.push(uploadFile);
@@ -56,7 +55,7 @@ const parse = (event) => new Promise((resolve, reject) => {
         reject(error);
     });
 
-    busboy.on('finish', () => {
+    busboy.on('close', () => {
         resolve(result);
     });
 
